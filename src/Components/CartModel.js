17f99fefef404cb41bd86ffc281e840f6cf78ff8
sophiasundar <sophiagifta17@gmail.com>
2023-07-02 00:10:43 +0530

    commit

---
+++ src/Components/CartModel.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+
+
+const Backdrop = (props) => {
+    return <div className="backdrop" onClick={props.onclick}></div>;
+  };
+
+  const ModalOverlay = (props) => {
+    return (
+      <div className="cart">{props.value.map((ele) => (<Cartitem key={ele.id} value={ele} onremove={props.onremove}/>))}
+        <div><button className="close btn-primary" onClick={props.onclick}>Close</button></div>
+      </div>
+    );
+  };
+
+  const Cartmodel = (props) => {
+    return(
+        <React.Fragment>
+            {
+                ReactDom.createPortal(
+                    <Backdrop onclick={props.onclick} />,
+                    document.getElementById("overlay")
+                  )
+            }
+
+            {
+                ReactDom.createPortal(
+                    <ModalOverlay value={props.value} onclick={props.onclick} onremove={props.onremove} />,
+                    document.getElementById("overlay")
+                  )
+            }
+        </React.Fragment>
+    )
+  }
+
+  export default Cartmodel;
---
+++ src/Components/Cartitems.js
@@ -0,0 +1,21 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import ""
+
+   const Cartitem = (props) => {
+    const removehandler= ()=>{
+      props.onremove(props.value);
+    }
+
+
+    return (
+        <li className="cart-item bg-light text-dark" key={props.value.id}>
+          <div >
+            <h2>{props.value.name}</h2>
+            <h4>{props.value.price}</h4>
+          </div>
+          <div><button className="btn-danger" onClick={removehandler}>Remove</button></div>
+        </li>
+      );
+}
+
+export default Cartitem;
