9dba51e0dd2755fadfa43fe5bf62918f79266fa8
sophiasundar <sophiagifta17@gmail.com>
2023-07-17 22:24:26 +0530

    commit

--- src/Components/Cardss.js
+++ src/Components/Cardss.js
@@ -8,17 +8,22 @@ const Cardss=({element})=>{
     
     const{name,price}=element  
 
+
+  //   const addtocart = ()=>{
+  //     element.onclick(element.item);
+  // }
     return(
         <>
-          <li className="cart-item bg-light text-dark" >
-          <div>
+        <div className="col-6 col-md-4 col-lg-3 p-3">
+          <div className="cborder mb-5 text-center pb-4">
           <div className="img" style={{ height: 150 }}>450 X300</div>
             <h2>{name}</h2>
             <h3><Rating></Rating></h3>
             <h4>{price}</h4>
+            <button className="btn-danger" >Add to cart</button>
+            {/* <button className="btn-danger" disabled ={element.disabled === "false" ? false : true} onClick={addtocart}><span>{element.disabled === "false" ? "Add to cart" : "Added to cart"}</span></button> */}
           </div>
-          <div><button className="btn-danger" >Remove</button></div>
-        </li>
+        </div>
         </>
     )
 }
